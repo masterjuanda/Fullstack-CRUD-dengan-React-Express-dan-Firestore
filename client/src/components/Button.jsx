f99c469eca579c68577f21f346f8dbbda9d2b61c
Master Juanda <masterjuandasirait01@gmail.com>
2025-10-29 22:00:26 +0700

    menambahkan 3 file didalam folder components

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({
+  children,
+  onClick,
+  variant = "primary",
+  type = "button",
+}) => {
+  return (
+    <button className={`btn btn-${variant}`} onClick={onClick} type={type}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
